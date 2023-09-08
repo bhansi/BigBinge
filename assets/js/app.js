93411874935e1fbd398b4d3c417b70a7b47db19f
Baljot Hansi <baljotshansi@gmail.com>
2023-09-07 21:19:23 -0300

    Replaced episode length criteria with series rating criteria. Added logic to help display more series per search result.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,70 +4,83 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    const episodeLengthChecked = document.getElementById("cbEpisodeLength").checked;
     const episodeQuantityChecked = document.getElementById("cbEpisodeQuantity").checked;
+    const seriesRatingChecked = document.getElementById("cbSeriesRating").checked;
     const episodeGenreChecked = document.getElementById("cbEpisodeGenre").checked;
 
-    const episodeLength = document.getElementById("nbrEpisodeLength").value;
     const episodeQuantity = document.getElementById("nbrEpisodeQuantity").value;
+    const seriesRating = document.getElementById("nbrSeriesRating").value;
     const episodeGenre = document.getElementById("ddEpisodeGenre").value;
-
+    const resultsDiv = document.getElementById("results");
+    
+    // Clear past search results for current search results
+    resultsDiv.innerHTML = "";
+    
+    let page = 0;
+    let numberOfDisplayedSeries = 0;
+    let doneDisplaying = false;
+    
     let query = "";
     if (episodeGenreChecked) {
       query += `&with_genres=${episodeGenre}`;
     }
+    if (seriesRatingChecked) {
+      query += `&vote_average.gte=${seriesRating}`;
+    }
 
-    const apiKey = "3e1cf70d880acb18f154700af5ac63f8";
-    const url = "https://api.themoviedb.org/3/discover/tv?include_adult=true&include_null_first_air_dates=true&language=en-US&screened_theatrically=true&sort_by=popularity.desc&with_genres=10759" + query + "&api_key=" + apiKey;
-      
     try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
+      while(!doneDisplaying) {
+          page++;
+          const apiKey = "3e1cf70d880acb18f154700af5ac63f8";
+          const url = "https://api.themoviedb.org/3/discover/tv?include_adult=true&include_null_first_air_dates=true&language=en-US&sort_by=popularity.desc" + query + "&page=" + page + "&api_key=" + apiKey;
+          
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data = await response.json();
+
+          let shows = data.results;
+
+          shows = await Promise.all(shows.map(async show => {
+            const seriesUrl = `https://api.themoviedb.org/3/tv/${show.id}?language=en-US&api_key=${apiKey}`;
+            const seriesResponse = await fetch(seriesUrl);
+            const seriesData = await seriesResponse.json();
+            show.total_episodes = seriesData.number_of_episodes;
+            show.rating = seriesData.vote_average;
+            return show;
+          }));
+
+          if(shows.length < 1) { break; } // Stop calling API if there are no more shows
+
+          if (episodeQuantityChecked) {
+            shows = shows.filter(show => show.total_episodes >= episodeQuantity);
+          }
+
+          shows.forEach(show => {
+            if(!doneDisplaying) {
+              const card = document.createElement("div");
+              card.className = "bg-white rounded overflow-hidden shadow-lg p-4";
+              card.innerHTML = `
+                <img class="w-full" src="https://image.tmdb.org/t/p/w500${show.poster_path}" alt="${show.name}">
+                <div class="px-6 py-4">
+                  <div class="font-bold text-xl mb-2">${show.name}</div>
+                  <p class="text-gray-700 text-base">${show.overview}</p>
+                </div>
+              `;
+              resultsDiv.appendChild(card);
+            }
+
+            numberOfDisplayedSeries++;
+            if(numberOfDisplayedSeries === 18) {
+              doneDisplaying = true;
+            }
+          });
+        }
       }
-      const data = await response.json();
-
-      console.log(data); 
-      let shows = data.results;
-
-      // if (episodeLengthChecked) {
-      //   shows = shows.filter(show => show.episode_run_time[0] >= episodeLength);
-      // }
-
-      // if (episodeQuantityChecked) {
-      //   shows = shows.filter(show => show.episode_number >= episodeQuantity);
-      // }
-
-      if (episodeQuantityChecked) {
-        // Fetch total number of episodes for each show and filter
-        shows = await Promise.all(shows.map(async show => {
-          const seriesUrl = `https://api.themoviedb.org/3/tv/${show.id}?language=en-US&api_key=${apiKey}`;
-          const seriesResponse = await fetch(seriesUrl);
-          const seriesData = await seriesResponse.json();
-          show.total_episodes = seriesData.number_of_episodes;
-          return show;
-        })).then(shows => shows.filter(show => show.total_episodes >= episodeQuantity));
+      catch (error) {
+        console.error("Fetch Error:", error);
       }
-
-      const resultsDiv = document.getElementById("results");
-      resultsDiv.innerHTML = "";
-
-      shows.forEach(show => {
-        const card = document.createElement("div");
-        card.className = "bg-white rounded overflow-hidden shadow-lg p-4";
-        card.innerHTML = `
-          <img class="w-full" src="https://image.tmdb.org/t/p/w500${show.poster_path}" alt="${show.name}">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">${show.name}</div>
-            <p class="text-gray-700 text-base">${show.overview}</p>
-          </div>
-        `;
-        resultsDiv.appendChild(card);
-      });
-    }
-    catch (error) {
-      console.error("Fetch Error:", error);
-    }
   });
 });
   
